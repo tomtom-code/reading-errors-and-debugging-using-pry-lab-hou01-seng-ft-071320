20dc93c7692982299b73b5dcf2ccd862ac439fcb
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-08 17:04:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -6,9 +6,9 @@ def snake_it_up(string)
   snake_string = " "
   
   if string[0] == "s"
-    
+   # binding.pry
     10.times "s" + string
-    
+    # WRONG NUMBER OF ARGUMENTS (given 1, expected 0)
   else
     string
   
