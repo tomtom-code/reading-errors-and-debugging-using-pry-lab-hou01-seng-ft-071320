f4219ea28db4db84c8e393e93171261ad575f81b
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-08 17:33:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,14 +1,14 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def generate_star_date
-  start_date = (rand(100000) + 400000) / 10.0
-  binding.pry 
-  return start_date
+  (rand(100000) + 400000) / 10.0
+   
+  
 end
 
 
 def state_log(star_date)
-  puts "Captain's Log, star date #{star_date}."
+  "Captain's Log, star date #{star_date}."
   
 end
 
